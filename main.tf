559ba8e2af06a522928716cab5b44dee66ae11a2
Bikram Agrawal <138153745+BikramAgrawal@users.noreply.github.com>
2025-06-15 00:08:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "devops-rg"
-  location = "Central India"
+  name     = "devops-rg2"
+  location = "East US"
 }
 
 resource "azurerm_service_plan" "plan" {
